c9eeecdbb4d06e77204e47408cc77926a7721fd1
Emilion <emil.kopev@gmail.com>
2016-12-28 16:24:55 +0200

    [fix] build issues and wrong declarations

--- src/ng2-window-manager.component.ts
+++ src/ng2-window-manager.component.ts
@@ -2,7 +2,11 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'window',
-    templateUrl: './ng2wm-window-template.html'
+    template: `<div class="row">
+    <header class="col-md-12">window manager header goes here</header>
+    <section class="col-md-12"></section>
+    <footer class="col-md-12"></footer>
+        </div>`
 })
 export class NG2WMWindow {
 
